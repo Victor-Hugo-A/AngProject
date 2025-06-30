4648631adaed08957c3d598da7569bc30f95ff1a
Victor-Hugo-A <victorpht.vh@gmail.com>
2025-06-30 13:20:17 -0300

    first creating

---
+++ src/app/pages/login/login.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Header } from '../../componente/header/header';
+
+@Component({
+  selector: 'app-login',
+  imports: [
+    Header
+  ],
+  templateUrl: './login.html',
+  styleUrl: './login.scss'
+})
+export class Login {
+
+}
